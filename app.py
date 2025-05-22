f97a3c8b554ef459749008fefc6315414c2a2881
Nafisatulwidiya <nafisatulwidiyasari@gmail.com>
2025-05-22 08:43:03 +0700

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask
+import streamlit as st
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello, GitHub! This is a simple Flask app."
-
-if __name__ == "__main__":
-    app.run(debug=True)
+st.title("Hello, World!")
